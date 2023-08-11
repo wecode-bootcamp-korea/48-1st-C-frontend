58b2098fd0d01e94ac1eaf4a4faeac4c0760bda6
llinjae <dldlswognqh@naver.com>
2023-08-11 17:21:27 +0900

    Add : 초기 세팅 완료

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div>
+      <Link to="/">hihihihihi</Link>
+    </div>
+  );
+};
+
+export default Login;
