33365721dc013a4ff83f5764ee148bf702adaf3c
llinjae <dldlswognqh@naver.com>
2023-08-11 18:28:52 +0900

    Add : 초기 세팅 추가

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import PostList from './pages/PostList/PostList';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/post-list" element={<PostList />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/config.js
@@ -0,0 +1,2 @@
+const BASE_URL = 'http://10.58.5.151:8000';
+  B  
