20e1abeae069dc7118e6f60955149c292984f1c0
llinjae <dldlswognqh@naver.com>
2023-08-14 18:39:22 +0900

    Add : Feed Detail UI 구현

---
+++ src/pages/PostList/components/CommentItem.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './CommentItem.scss';
+
+const CommentItem = ({ feedData, feedDate }) => {
+  return (
+    <>
+      <form className="inputForm">
+        <input
+          type="text"
+          placeholder="댓글을 작성해주세요."
+          className="feedInput"
+        />
+        <button className="postBtn">댓글 게시</button>
+      </form>
+
+      <div className="commentContainer">
+        <div className="commentProfileBox">
+          <img
+            className="commentProfileImg"
+            src={feedData.userImage}
+            alt="프로필 이미지"
+          />
+          <div className="commentRightBox">
+            <div className="commentDetailUserInfo">
+              <p className="userName">
+                <b>{feedData.nickname}</b>
+              </p>
+              <div className="commentProfileBoxRight">
+                <p className="date">{feedDate}</p>
+                <button className="deleteBtn">삭제</button>
+                <button className="editBtn">수정</button>
+              </div>
+            </div>
+            <p className="userCommentBox">
+              sdafasddsafasdfasdfsafasfasdfjssdafasddsafasdfasdfsafasfasdfjssdafasddsafasdfasdfsafasfasdfjssdafasddsafasdfasdfsafasfasdfjssdafasddsafasdfasdfsafasfasdfjssdafasddsafasdfasdfsafasfasdfjs
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CommentItem;
--- src/pages/PostList/components/Feed.jsx
+++ src/pages/PostList/components/Feed.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Feed.scss';
-import FeedItem from './FeedItem';
+import CommentItem from './CommentItem';
 
 const Feed = ({ feedData, handleRemove }) => {
   const [heartToggle, setHeartToggle] = useState(false);
@@ -19,37 +19,52 @@ const Feed = ({ feedData, handleRemove }) => {
   };
 
   return (
-    <div className="feed">
-      <div className="feedProfileBox" onClick={handleHideFeedContent}>
-        <div className="feedProfileBoxLeft">
-          <img
-            className="profileImg"
-            src={feedData.userImage}
-            alt="프로필 이미지"
-          />
-          <p className="userName">
-            <b>{feedData.nickname}</b>
-          </p>
+    <>
+      <div className="feed" onClick={handleHideFeedContent}>
+        <div className="feedProfileBox">
+          <div className="feedProfileBoxLeft">
+            <img
+              className="profileImg"
+              src={feedData.userImage}
+              alt="프로필 이미지"
+            />
+            <p className="userName">
+              <b>{feedData.nickname}</b>
+            </p>
+          </div>
+          <div className="feedProfileBoxRight">
+            <p className="date">{feedDate}</p>
+            <button
+              className="deleteBtn"
+              onClick={() => handleRemove(feedData.id)}
+            >
+              삭제
+            </button>
+          </div>
         </div>
-        <div className="feedProfileBoxRight">
-          <p className="date">{feedDate}</p>
-          <button
-            className="deleteBtn"
-            onClick={() => handleRemove(feedData.id)}
-          >
-            삭제
-          </button>
+        <div className="feedContentBox">
+          <p className="feedContent">{feedData.content}</p>
+        </div>
+        <div className="feedDescriptionBox">
+          <div className="feedDescriptionTop">
+            <p>좋아요 {like}</p>
+            <p>댓글 00</p>
+          </div>
+          <img
+            onClick={handleHeartToggle}
+            className="feedHeartImg"
+            src={
+              heartToggle
+                ? process.env.PUBLIC_URL + '/images/fillheart.svg'
+                : process.env.PUBLIC_URL + '/images/heart.svg'
+            }
+            alt="좋아요 버튼"
+          />
         </div>
       </div>
-      {!hideFeedContent && (
-        <FeedItem
-          like={like}
-          heartToggle={heartToggle}
-          handleHeartToggle={handleHeartToggle}
-          feedData={feedData}
-        />
-      )}
-    </div>
+
+      <CommentItem feedData={feedData} feedDate={feedDate} />
+    </>
   );
 };
 
